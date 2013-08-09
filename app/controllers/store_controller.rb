ba23b75772e39794d47550f471c9884ab1c0ff2b
byzgaev <artem.byzgaev@kodep.ru>
2013-08-09 09:16:10 +0400

    Unit_D

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products=Product.order(:title)
+    session[:counter].nil? ? session[:counter]=0 : session[:counter]+=1
+    @counter_label="This page was showed  #{session[:counter]} times" if session[:counter]>5
   end
 end
